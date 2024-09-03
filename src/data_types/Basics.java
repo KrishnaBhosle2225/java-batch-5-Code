15c182e206fa53072aae7873b02046555c39bf5e
Krishna Bhosle <kbhosle2000@gmail.com>
2024-09-03 20:48:16 +0530

    Day one code basics of Java Programming

---
+++ src/data_types/Basics.java
@@ -0,0 +1,28 @@
+package data_types;
+
+public class Basics {
+
+    public static void main(String[] args) {
+
+        //byte data type 1 byte (8 bit)
+//        range -128 to 127
+        byte a = 12;
+        System.out.println(a);
+
+//        short takes memory of size 2 bytes
+        short b = 128;
+        System.out.println(b);
+
+        //int is of 4byte memory (4*8)
+        int roll_no = 324;
+        System.out.println(roll_no);
+
+        int money = 498357894;
+        System.out.println(money);
+
+//        long is a another data types -> 8 bytes (8*8)
+        long views = 893825676574343l;
+
+
+    }
+}
