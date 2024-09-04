72058382b87cc9f9c018ec667b3d2b71ea959963
Krishna Bhosle <kbhosle2000@gmail.com>
2024-09-04 20:38:52 +0530

    day 2 code data types, identifiers, typecasting

---
+++ src/Day_2/DataTypes2.java
@@ -0,0 +1,55 @@
+package Day_2;
+
+public class DataTypes2 {
+
+    public static void main(String[] args) {
+
+        //float
+        float rupees = 10.5f;
+        float cgpa = 7.5f;
+//        System.out.println("I have " + rupees + " rupees");
+//        System.out.println("I have " + cgpa + " cgpa grades");
+
+        //double
+        double currency = 433.78492;
+//        System.out.println(currency);
+
+//        char
+        char alphabet = 'D';
+        char abc = 'S';
+//        System.out.println(alphabet);
+
+        //boolean
+        boolean response = true;
+//        System.out.println("I going to gym" + response);
+
+        //Non-primitive Data type
+//        String name = "Ascii education";
+//        System.out.println("I am leanrning Java at " + name);
+
+//        int[] data = {1, 2,3,4,5,1,2,3,4,5};
+
+//        String name= "Ascii education";
+        String work = "Teaching Programming languages";
+
+//        System.out.print(name);
+//        System.out.println(work);
+//
+//        System.out.println(name);
+//        System.out.println(work);
+
+        String Name = "Neha";
+        int _id = 100;
+        int $usd = 100;
+        boolean no = false;
+
+        String dgkfbkfgbkfgbfgbfgfgbfgbgbjggbfjhgbfjgbdfjgbfjgfdgjbfg = "fsdf";
+        System.out.println(dgkfbkfgbkfgbfgbfgfgbfgbgbjggbfjhgbfjgbdfjgbfjgfdgjbfg);
+
+        int roll_no = 10;
+        System.out.println(roll_no);
+
+//        int @a = 323;
+
+    }
+}
