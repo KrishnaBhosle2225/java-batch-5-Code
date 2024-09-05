def539c710c86b86114cf5e8b818ed7accbc98b5
Krishna Bhosle <kbhosle2000@gmail.com>
2024-09-05 20:27:03 +0530

    Day 3 Operators code

---
+++ src/Day_3/Operators.java
@@ -0,0 +1,70 @@
+package Day_3;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        //arithmatic operator (+, - , *, /, %)
+        //*, / home work
+        int a = 61;
+        int b = 20;
+        int c = a%b;
+//        System.out.println("a and b : " + c);
+
+        //addition assignment (+=)
+        int d = 10 + 10;//20
+        d += 10;//d = 20+ 10;
+        System.out.println(d);
+
+        //substraction assignment(-=)
+        int e = 10;
+//        e -= 10;
+        e = e - 10;
+//        System.out.println(e);
+
+        //multiplication assignment (*=)
+        int f = 5;
+        f *= 20;
+//        f = f * 20;
+//        System.out.println(f);
+
+        //remaining home work
+
+
+        //comparision operator
+        int g = 10;
+        int h = 20;
+
+        //less than operator
+        System.out.println(g<h);
+
+        //greater than
+        System.out.println(g > h);
+
+        //less than equal to
+        int x = 20;
+        int y = 10;
+        System.out.println(x<=y);//20<=10
+
+        //greater than equal to (>=) HW
+
+        //ternary operator
+        System.out.println((10 < 18) ? "No" : "Yes");
+
+        //Unary operators (++, --)
+        int J = 20;
+        J--;//21
+//        J = J+1;//22
+        System.out.println(J);
+
+
+
+
+
+
+
+
+
+
+    }
+}
